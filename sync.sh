0c7363d2e19ec0ae4fd022e340e94d70f80307e4
jaeyoung.choi <lactrious@gmail.com>
2019-08-08 12:46:04 +0900

    Invalidate the cache of "/"

--- sync.sh
+++ sync.sh
@@ -49,7 +49,7 @@ fi
 
 aws cloudfront create-invalidation \
   --distribution-id "${DISTRIBUTION_ID}" \
-  --paths ${TARGET_PATHS}
+  --paths ${TARGET_PATHS} "/"
 
 if [ $? -ne 0 ]; then
   echo "Cache invalidation is failed."
